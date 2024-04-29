864dca03f7496f2e108567885f902800efb83195
chris <actuallychrismendez@gmail.com>
2024-04-28 20:08:05 -0700

    deployed snake object

--- app/src/main/java/com/gamecodeschool/csc133project/Collidable.java
+++ app/src/main/java/com/gamecodeschool/csc133project/Collidable.java
@@ -1,6 +1,7 @@
 package com.gamecodeschool.csc133project;
 import android.graphics.Rect;
 import android.graphics.Point;
+
 public interface Collidable {
     boolean checkCollision(Point location);
 }
